82bf771566fe646e742e350d1debf618a56d1ca2
Vu Hoang <vulego2004@gmail.com>
2025-02-03 04:29:55 +0700

    Time: 13 ms (76.26%), Space: 64.3 MB (27.39%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -0,0 +1,23 @@
+class RecentCounter {
+public:
+
+    queue<int> queue;
+
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        while (queue.empty() == false && queue.front() < t - 3000 ){
+            queue.pop();
+        }
+        queue.push(t);
+        return queue.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
