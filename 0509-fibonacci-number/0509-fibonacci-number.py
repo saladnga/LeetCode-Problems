269a9d6a9647b2cd6b426250b198fcf6ec982d3c
An Vu Hoang <vulego2004@gmail.com>
2025-10-22 02:03:58 -0500

    Time: 31 ms (93.24%), Space: 17.7 MB (41.37%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        cache = {}
+        def recur_fib(N):
+            if N in cache:
+                return cache[N]
+            if N < 2:
+                result = N
+            else:
+                result = recur_fib(N - 1) + recur_fib(N - 2)
+            cache[N] = result
+            return result
+        return recur_fib(n)
