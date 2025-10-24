cbd9850d3ae1abd0e2cea47dc0583a0d404412bd
An Vu Hoang <vulego2004@gmail.com>
2025-10-24 00:50:12 -0500

    Time: 0 ms (100%), Space: 17.8 MB (63.22%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,13 +1,7 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # relation: min(cost[i - 1] + dp(i - 1), dp(i - 2), cost[i - 2]) -> min of 99 and 98
-        memo = {}
-        def dp(i):
-            if i <= 1:
-                return 0
-            if i in memo:
-                return memo[i]
-            memo[i] = min(cost[i - 1] + dp(i - 1), cost[i - 2] + dp(i - 2))
-            return memo[i]
-
-        return dp(len(cost))
+        n = len(cost)
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            dp[i] = min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2])
+        return dp[n]
