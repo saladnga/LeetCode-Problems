9c6638fd860d40112048f7744378e5da3b637c0d
An Vu Hoang <vulego2004@gmail.com>
2025-10-25 09:45:21 -0500

    Time: 4 ms (8.16%), Space: 18.3 MB (6.46%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def dp(row, col):
+            if row == 0 and col == 0:
+                return 1
+            ans = 0
+            if row > 0:
+                ans += dp(row - 1, col)
+            if col > 0:
+                ans += dp(row, col - 1)
+            return ans
+        return dp(m - 1, n - 1)
