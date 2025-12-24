d2e02cae8018a36cebe787c15b941d12252748f7
Vu Hoang <vulego2004@gmail.com>
2025-12-24 10:44:36 -0600

    Time: 0 ms (100%), Space: 17.2 MB (98.61%) - LeetHub

--- 3280-convert-date-to-binary/3280-convert-date-to-binary.py
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.py
@@ -1,11 +1,3 @@
 class Solution:
     def convertDateToBinary(self, date: str) -> str:
-        def convert(n):
-            if n == 0:
-                return "0"
-            res = ""
-            while n > 0:
-                res = str(n % 2) + res
-                n //= 2
-            return res
-        return "-".join(convert(int(x)) for x in date.split("-"))
+        return "-".join(bin(int(x))[2:] for x in date.split("-"))
