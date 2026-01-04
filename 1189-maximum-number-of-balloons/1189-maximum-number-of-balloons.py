1e9fd0c0fc510c2383b170535a744dc3b8ea93ba
Vu Hoang <vulego2004@gmail.com>
2026-01-04 03:47:04 -0600

    Time: 3 ms (60.98%), Space: 17.2 MB (92.16%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
+        min_count = float('inf')
+        # ToDo: Write Your Code Here.
         count = {}
         for c in text:
             if c not in count:
                 count[c] = 0
             count[c] += 1
-        b_count = count.get('b', 0)
-        a_count = count.get('a', 0)
-        l_count = count.get('l', 0) // 2
-        o_count = count.get('o', 0) // 2
-        n_count = count.get('n', 0)
-        return min(b_count, a_count, l_count, o_count, n_count)
+        min_count = min(min_count, count.get('b', 0))
+        min_count = min(min_count, count.get('a', 0))
+        min_count = min(min_count, count.get('l', 0) // 2)
+        min_count = min(min_count, count.get('o', 0) // 2)
+        min_count = min(min_count, count.get('n', 0))
+        
+        return min_count
