c964aeffbf4138c905570aff4f2552f02bf97bd4
Vu A. Hoang <vulego2004@gmail.com>
2026-02-03 15:58:09 -0600

    Time: 0 ms (100%), Space: 19.2 MB (48.24%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -13,11 +13,11 @@ class Solution:
 
         while queue:
             current_length = len(queue)
-            ans.append(queue[-1].val)
             for _ in range(current_length):
                 node = queue.popleft()
                 if node.left != None:
                     queue.append(node.left)
                 if node.right != None:
                     queue.append(node.right)
+            ans.append(node.val)
         return ans
