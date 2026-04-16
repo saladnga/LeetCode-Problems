730dade6b6dffa5fddac48faaca625bed68b6779
Vu A. Hoang <vulego2004@gmail.com>
2026-04-15 19:25:23 -0500

    Time: 151 ms (88.29%), Space: 27 MB (42.87%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,5 +1,9 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
         stack = []
         answer = [0] * len(temperatures)
         for i in range(len(temperatures)):
@@ -8,3 +12,4 @@ class Solution:
                 answer[j] = i - j
             stack.append(i)
         return answer
+        
