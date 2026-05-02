6c9ed27a1e6f12ec5daa339d649f81bd045a87a1
Vu A. Hoang <vulego2004@gmail.com>
2026-05-02 11:04:25 -0500

    Time: 309 ms (30.72%), Space: 67.7 MB (93.68%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    activity = activity.groupby(["player_id"])['event_date'].min().reset_index()
+    return activity.rename(columns={'event_date': 'first_login'})
