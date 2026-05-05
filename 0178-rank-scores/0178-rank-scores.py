1382d67a4a8aac8987dc56b1e9285c97fc9fc03b
Vu A. Hoang <vulego2004@gmail.com>
2026-05-05 17:27:41 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.py
+++ 0178-rank-scores/0178-rank-scores.py
@@ -2,14 +2,5 @@ import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
     scores = scores.sort_values(by='score', ascending=False)
-    scores["rank"] = 0
-    curr_rank = 0
-    prev_score = None
-    for i in scores.index:
-        curr_score = scores.loc[i, 'score']
-        if prev_score != curr_score:
-            curr_rank += 1
-        scores.loc[i, 'rank'] = curr_rank
-        prev_score = curr_score
+    scores['rank'] = scores['score'].rank(method='dense', ascending=False)
     return scores[['score', 'rank']]
-        
