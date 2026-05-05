f10c01f1574842cbd65ee3603db546eb5811660e
Vu A. Hoang <vulego2004@gmail.com>
2026-05-05 17:04:38 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    employee = employee[employee['salary'] == employee.groupby('departmentId')['salary'].transform('max')]
-    employee = employee.rename(columns={'name': 'Employee', 'salary': 'Salary'})
-    employee = employee.merge(department, left_on='departmentId', right_on='id')
-    employee = employee.rename(columns={'name': 'Department'})
-    return employee[['Department', 'Employee', 'Salary']]
+    mask = employee['salary'] == employee.groupby('departmentId')['salary'].transform('max')
+    res = employee[mask].copy()
+    res = res.merge(
+        department,
+        left_on='departmentId',
+        right_on='id',
+        suffixes=('_emp', '_dept')
+    )
+    res = res.rename(columns={'name_emp': 'Employee', 'name_dept': 'Department', 'salary': 'Salary'})
+    return res[['Department', 'Employee', 'Salary']]
