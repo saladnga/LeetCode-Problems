948c30ffafa173856d717f9037469db70e6151d2
Vu A. Hoang <vulego2004@gmail.com>
2026-05-05 16:24:03 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    employee = employee['salary'].drop_duplicates().sort_values(ascending=False)
-    if len(employee) < 2:
+    df = employee['salary'].drop_duplicates().sort_values(ascending=False)
+    if len(df) < 2:
         return pd.DataFrame({f"SecondHighestSalary": [None]})
     else:
-        result = employee.iloc[1]
+        result = df.iloc[1]
         return pd.DataFrame({f"SecondHighestSalary": [result]})
