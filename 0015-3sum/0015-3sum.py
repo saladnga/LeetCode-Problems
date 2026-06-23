9479f98cdb700f04d267c57af03cc0a9c5e3a4af
Vu A. Hoang <vulego2004@gmail.com>
2026-06-22 23:49:51 -0500

    Time: 601 ms (72.98%), Space: 22.4 MB (18.63%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,23 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        res = []
         nums.sort()
-        triplets = []
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i-1]:
                 continue
-            left = i + 1
-            right = len(nums) - 1
+            left, right = i + 1, len(nums) - 1
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
-                    triplets.append([nums[i], nums[left], nums[right]])
-                    left_val = nums[left]
-                    right_val = nums[right]
-                    while left < right and nums[left] == left_val:
-                        left += 1
-                    while left < right and nums[right] == right_val:
-                        right -= 1
-                elif total < 0:
+                curr = num + nums[left] + nums[right]
+                if curr < 0:
                     left += 1
-                else:
+                elif curr > 0:
                     right -= 1
-        return triplets
+                else:
+                    res.append([num, nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return res
